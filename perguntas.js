4693a39c92258132243745dd219407a17bc1ff84
Mariana1116 <160602641+Mariana1116@users.noreply.github.com>
2024-10-10 23:45:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,65 @@
+criarCARTAO(
+    'O que é Java Script?',
+    'Programação',
+    'É uma linguagem de programação'
+)
+criarCARTAO(
+    'O que é um bug?',
+    'Programação',
+    'É um erro ou falha no código'
+)
+criarCARTAO(
+    'O que é CSS?',
+    'Programação',
+    'É uma linguagem de estilização'
+)
+criarCARTAO(
+    'O que é uma variável?',
+    'Programação',
+    'Espaço na memória para armazenar dados'
+)
+criarCARTAO(
+    'O que é um loop?',
+    'Programação',
+    'Repetição de um bloco de código'
+)
+criarCARTAO(
+    'O que é um algoritmo?',
+    'Programação',
+    'Conjunto de passos para resolver um problema'
+)
+criarCARTAO(
+    'O que faz a função ``print´´?',
+    'Programação',
+    'Exibe informações na tela'
+)
+criarCARTAO(
+    'O que são variações globais?',
+    'Programação',
+    'Variáveis acessíveis em todo o programa, em qualquer parte do código'
+)
+criarCARTAO(
+    'Quando ocorreu a independência do Brasil?',
+    'História',
+    '7 de setembro de 1822'
+)
+criarCARTAO(
+    'Qual é o maior continente?',
+    'Geografia',
+    'Ásia'
+)
+criarCARTAO(
+    'O que um número primo?',
+    'Matemática',
+    'Número que só é divisível por 1 e por ele mesmo'
+)
+criarCARTAO(
+    'O que são substantivos?',
+    'Língua Portuguesa',
+    'Palavras que nomeiam coisas, pessoas ou lugares'
+)
+criarCARTAO(
+    'Como se diz ``Olá´´ em inglês?',
+    'Língua inglesa',
+    'Hello'
+)
